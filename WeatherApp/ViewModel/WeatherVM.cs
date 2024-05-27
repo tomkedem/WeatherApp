6bff1730763dc615a5bea4160a9491f722879c7e
Tomer Kedem <tompha1@hotmail.com>
2024-05-27 18:28:10 +0300

    Using Design Mode Bindings

--- WeatherApp/ViewModel/WeatherVM.cs
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -38,6 +38,35 @@ public class WeatherVM : INotifyPropertyChanged
         }
     }
 
+    public WeatherVM()
+    {
+        if(DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+        {
+            SelectedCity = new City
+            {
+                LocalizedName = "Tel Aviv",
+            };
+            CurrentConditions = new CurrentConditions
+            {
+                WeatherText = "Sunny",
+                Temperature = new Temperature
+                {
+                    Metric = new Units
+                    {
+                        Value = 25,
+                        Unit = "C"
+                    },
+                    Imperial = new Units
+                    {
+                        Value = 77,
+                        Unit = "F"
+                    }
+                }
+            };
+        }
+       
+    }
+
     public event PropertyChangedEventHandler? PropertyChanged;
     private void OnPropertyChanged(string propertyName)
     {
